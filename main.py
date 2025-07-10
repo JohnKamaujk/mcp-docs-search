cea915799e415f875e863740d70ef4a399f8bae2
JohnKamaujk <johnjkkamau@gmail.com>
2025-07-10 17:11:05 +0300

    feat: get docs tool

--- main.py
+++ main.py
@@ -43,7 +43,11 @@ async def search_web(query: str) -> dict:
         return {"organic": []}
     
 async def fetch_url(url: str):
-  async with httpx.AsyncClient() as client:
+    """
+    Given a URL, fetch their text content using BeautifulSoup.
+    Returns text_content
+    """
+    async with httpx.AsyncClient() as client:
         try:
             response = await client.get(url, timeout=30.0)
             soup = BeautifulSoup(response.text, "html.parser")
@@ -52,9 +56,32 @@ async def fetch_url(url: str):
         except httpx.TimeoutException:
             return "Timeout error"
 
-def main():
-    print("Hello from mcp-docs-search!")
+@mcp.tool()  
+async def get_docs(query: str, library: str):
+    """
+    Search the latest docs for a given query and library.
+    Supports langchain, openai, and llama-index.
+
+    Args:
+        query: The query to search for (e.g. "Chroma DB")
+        library: The library to search in (e.g. "langchain")
+
+    Returns:
+        Text from the docs
+    """
+    if library not in docs_urls:
+        raise ValueError(f"Library {library} not supported by this tool")
+  
+    query = f"site:{docs_urls[library]} {query}"
+    results = await search_web(query)
+    if len(results["organic"]) == 0:
+        return "No results found"
+    
+    text = ""
+    for result in results["organic"]:
+        text += await fetch_url(result["link"])
+    return text
 
 
 if __name__ == "__main__":
-    main()
+    mcp.run(transport="stdio")
